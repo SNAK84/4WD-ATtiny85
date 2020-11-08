709aa9640a83d50a58f80cdb880a1355ec2b30b7
SNAK84 <snak@snak84.ru>
2020-11-08 13:55:23 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
 #include <avr/eeprom.h>
 
+
+#include "ATtinySerialOut.h"
+
 uint8_t EEP_4WD_ON = 0;
 uint8_t EEP_4WD_LOOK = 1;
 
 #define PIN_4WD_ON 0
 #define PIN_4WD_LOOK 1
 #define PIN_4WD_ON_LED 2
+
 #define PIN_BTN_4WD A3
 #define PIN_RUCHNIK A2
 
-#define HOLD_BTN 300
-#define CLICK_BTN 15
+#define HOLD_BTN 1000
+#define CLICK_BTN 50
 
 int LastBtn = 0;
 
@@ -23,42 +27,26 @@ bool RUCHNIK_On = false;
 bool Btn_Press = false;
 bool Btn_4WD_Hold = false;
 
-
 long TimeLoop;
 
 void setup()
 {
 
-  /*Serial.begin(9600);
-  Serial.println("Start");*/
   pinMode(PIN_4WD_ON, OUTPUT);
   pinMode(PIN_4WD_LOOK, OUTPUT);
   pinMode(PIN_4WD_ON_LED, OUTPUT);
 
-/*
-digitalWrite(0,1);
-delay(1000);
-digitalWrite(1,1);
-delay(1000);
-digitalWrite(2,1);
-delay(1000);
-
-digitalWrite(0,0);
-delay(1000);
-digitalWrite(1,0);
-delay(1000);
-digitalWrite(2,0);
-delay(1000);
-*/
+  
   WD_On = eeprom_read_byte(&EEP_4WD_ON);
   WD_Look = eeprom_read_byte(&EEP_4WD_LOOK);
 }
 
 void loop()
 {
+
   long ThisMillis = millis();
 
-  if (analogRead(PIN_RUCHNIK) < 512)
+  if (!digitalRead(PIN_RUCHNIK))
   {
     RUCHNIK_On = true;
   }
@@ -67,7 +55,8 @@ void loop()
     RUCHNIK_On = false;
   }
 
-  int Btn_4WD = analogRead(PIN_BTN_4WD);
+  int Btn_4WD = digitalRead(PIN_BTN_4WD);
+  //int Btn_4WD = analogRead(PIN_BTN_4WD);
   if (LastBtn > Btn_4WD)
   {
     LastDownBtn = ThisMillis;
@@ -86,8 +75,9 @@ void loop()
     }
   }
 
-  // Подсчет времени удержания кнопки 
-  if (Btn_4WD < 900 && !Btn_4WD_Hold)
+  // Подсчет времени удержания кнопки
+  //if (Btn_4WD < 900 && !Btn_4WD_Hold)
+  if (!Btn_4WD && !Btn_4WD_Hold)
   {
     LastUpBtn = ThisMillis;
   }
@@ -130,17 +120,23 @@ void loop()
   else
   {
     // Запись состояния 4WD
-    digitalWrite(PIN_4WD_ON, WD_On);
-    digitalWrite(PIN_4WD_LOOK, WD_Look);
+    if (WD_On)
+      digitalWrite(PIN_4WD_ON, HIGH);
+    else
+      digitalWrite(PIN_4WD_ON, LOW);
+    if (WD_Look)
+      digitalWrite(PIN_4WD_LOOK, HIGH);
+    else
+      digitalWrite(PIN_4WD_LOOK, LOW);
   }
 
   // Включение подсветки если 4WD включен
-  if (WD_On)
+  /*if (WD_On)
   {
     digitalWrite(PIN_4WD_ON_LED, HIGH);
   }
   else
   {
     digitalWrite(PIN_4WD_ON_LED, LOW);
-  }
+  }*/
 }
